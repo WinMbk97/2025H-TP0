796809e864cc17a95b8a2309e426eaf42a60cc1b
WinMbk97 <winmabaka207@gmail.com>
2025-01-13 14:16:49 -0500

    reponse a la question du rayon

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
+air = math.pi * rayon**2
+print (air)
 
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+
+Prenom = "Winner"
+print (Prenom)
